d567563732d100e17a837e1cca11353a9d1faec2
Willex275 <willestrandberg14@gmail.com>
2018-09-05 16:18:04 +0200

    william strandberg programering

---
+++ src/Programering01/helloworld.java
@@ -0,0 +1,30 @@
+package Programering01;
+
+import java.util.Scanner;
+
+public class helloworld {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		
+		if(x>0 && y>0){
+			System.out.println(1);
+		}
+		if(x>0 && y<0){
+			System.out.println(4);
+		}
+		if(x<0 && y>0) {
+			System.out.println(2);
+		}
+		if(x<0 && y<0) {
+			System.out.println(3);
+		}
+		
+      } // END main
+	
+	
+} // END helloworld
