3a941e93b6024e4164d7e9ef4c80d4e549b1fc1b
Willex275 <willestrandberg14@gmail.com>
2018-09-12 10:25:21 +0200

    commit 4

--- src/Programering01/infromationomWilliam.java
+++ src/Programering01/infromationomWilliam.java
@@ -11,19 +11,19 @@ public class infromationomWilliam {
 	String namn = input.nextLine();
 	
 	System.out.println("Vad är ditt telefon nummer?");
-	String telefonnummmer = input.nextLine();
+	String telefonnummer = input.nextLine();
 	
 	System.out.println("Vilken stad bor du i");
 	String Stad = input.nextLine();
 	
 	System.out.println("vad är ditt postnummer?");
-	String namn = input.nextLine();
+	String postnummer = input.nextLine();
 	
 	System.out.println("Vilken adress bor du på?");
-	String namn = input.nextLine();
+	String adress = input.nextLine();
 	
 	System.out.println("Information:");
-	System.out.println(namn + " " + );
+	System.out.println(namn + " " + telefonnummer + " " + Stad + " " + postnummer + " " + adress );
 	
 	
 	
