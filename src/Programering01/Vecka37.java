28f0779e1c74f3a3c35b13cdc9ee7d254a564c7f
Willex275 <willestrandberg14@gmail.com>
2018-09-14 14:49:39 +0200

    commit 1

---
+++ src/Programering01/Vecka37.java
@@ -0,0 +1,38 @@
+package Programering01;
+
+import java.util.Scanner;
+
+public class Vecka37 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int max = 999999999;
+        int min = -99999999;
+	
+        
+        // läs in tal 1
+        int tal1 = input.nextInt();
+       
+        
+        int tal2 = input.nextInt();
+        
+        int tal3 = input.nextInt();
+        
+        int tal4 = input.nextInt();
+        
+        int tal5 = input.nextInt();
+        
+        int tal6 = input.nextInt();
+        
+        int tal7 = input.nextInt();
+        
+        int tal8 = input.nextInt();
+        
+        int tal9 = input.nextInt();
+        
+        int tal10 = input.nextInt();
+        		
+	}
+
+}
