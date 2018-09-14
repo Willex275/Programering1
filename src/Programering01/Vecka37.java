e54213fba16f04e5f77a50df5e534229afcc4127
Willex275 <willestrandberg14@gmail.com>
2018-09-14 14:59:32 +0200

    commit 2

--- src/Programering01/Vecka37.java
+++ src/Programering01/Vecka37.java
@@ -9,29 +9,42 @@ public class Vecka37 {
 		
 		int max = 999999999;
         int min = -99999999;
-	
+	     int medel = 0; 
         
-        // läs in tal 1
+        // läs in tal 1...
         int tal1 = input.nextInt();
        
-        
+        // läs in tal 2...
         int tal2 = input.nextInt();
         
+        // läs in tal 3...
         int tal3 = input.nextInt();
         
+     // läs in tal 4...
         int tal4 = input.nextInt();
         
+     // läs in tal 5...
         int tal5 = input.nextInt();
         
+     // läs in tal 6...
         int tal6 = input.nextInt();
         
+     // läs in tal 7...
         int tal7 = input.nextInt();
         
+     // läs in tal 8...
         int tal8 = input.nextInt();
         
+     // läs in tal 9...
         int tal9 = input.nextInt();
         
+     // läs in tal 10...
         int tal10 = input.nextInt();
+        
+        System.out.println("Minsta: " + min);
+        System.out.println("Största: " + max);
+        System.out.println("Medel: " + medel);
+        
         		
 	}
 
