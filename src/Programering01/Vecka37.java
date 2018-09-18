6fd1c7d0fb03cd107872c36752779e4bfb7c8d76
Willex275 <willestrandberg14@gmail.com>
2018-09-18 15:06:59 +0200

    commit 5

--- src/Programering01/Vecka37.java
+++ src/Programering01/Vecka37.java
@@ -15,41 +15,84 @@ public class Vecka37 {
         // läs in tal 2...
         int tal2 = input.nextInt();
         if (tal2 > max) {
-        	int max = tal2;
+        	max = tal2;
         }
-        
+        if (tal2 < min) {
+        	min = tal2;
+        }
         // läs in tal 3...
         int tal3 = input.nextInt();
-        
+        if (tal3 > max) {
+        	max = tal3;
+        }
+        if (tal3 < min) {
+        	min = tal3;
+        }
      // läs in tal 4...
         int tal4 = input.nextInt();
-        
+        if (tal4 > max) {
+        	max = tal4;
+        }
+        if (tal4 < min) {
+        	min = tal4;
+        }
      // läs in tal 5...
         int tal5 = input.nextInt();
-        
+        if (tal5 > max) {
+        	max = tal5;
+        }
+        if (tal5 < min) {
+        	min = tal5;
+        }
      // läs in tal 6...
         int tal6 = input.nextInt();
-        
+        if (tal6 > max) {
+        	max = tal6;
+        }
+        if (tal6 < min) {
+        	min = tal6;
+        }
      // läs in tal 7...
         int tal7 = input.nextInt();
-        
+        if (tal7 > max) {
+        	max = tal7;
+        }
+        if (tal7 < min) {
+        	min = tal7;
+        }
      // läs in tal 8...
         int tal8 = input.nextInt();
-        
+        if (tal8 > max) {
+        	max = tal8;
+        }
+        if (tal8 < min) {
+        	min = tal8;
+        }
      // läs in tal 9...
         int tal9 = input.nextInt();
-        
+        if (tal9 > max) {
+        	max = tal9;
+        }
+        if (tal9 < min) {
+        	min = tal9;
+        }
      // läs in tal 10...
         int tal10 = input.nextInt();
+        if (tal10 > max) {
+        	max = tal10;
+        }
+        if (tal10 < min) {
+        	min = tal10;
+        }
+        
         
         int summa = tal1 + tal2 + tal3 + tal4 + tal5 + tal6 + tal7 + tal8 + tal9 + tal10;
         
-        double medel1 = summa / 5; // 5 
-        double medel2 = summa / 5.0; //5,6
+       
         
         System.out.println("Minsta: " + min);
         System.out.println("Största: " + max);
-        System.out.println("Medel: " + medel);
+        System.out.println("Medel: " + summa/10);
         
         		
 	}
