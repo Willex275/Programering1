6a442590650370b493e73a2518cdb17562ca3740
Willex275 <willestrandberg14@gmail.com>
2018-09-19 15:58:57 +0200

    For loop räknar ut summan av alla tal under 100

---
+++ src/Programering01/Lektionsuppgifterv38.java
@@ -0,0 +1,16 @@
+package Programering01;
+
+public class Lektionsuppgifterv38 {
+
+	public static void main(String[] args) {
+
+int summa = 0; 
+		// Skriver summan av aLLA TAL UNDER 100
+		for (int i = 0; i < 100; i += 1) {
+			summa = summa + i;
+		}
+		System.out.println(summa);
+
+	}
+
+}
