eb479aa990166b7232c52c8c99b208c6844f2876
Willex275 <willestrandberg14@gmail.com>
2018-09-21 14:27:51 +0200

    commit helloworld/qudrant

--- src/Programering01/helloworld.java
+++ src/Programering01/helloworld.java
@@ -9,18 +9,18 @@ public class helloworld {
 		Scanner input = new Scanner(System.in);
 
 		int x = input.nextInt();
-		int k = input.nextInt();
+		int y = input.nextInt();
 
-		if (x > 0 && k > 0) {
+		if (x > 0 && y > 0) {
 			System.out.println(1);
 		}
-		if (x > 0 && k < 0) {
+		if (x > 0 && y < 0) {
 			System.out.println(4);
 		}
-		if (x < 0 && k > 0) {
+		if (x < 0 && y > 0) {
 			System.out.println(2);
 		}
-		if (x < 0 && k < 0) {
+		if (x < 0 && y < 0) {
 			System.out.println(3);
 		}
 
