d208822d9a299d1f57cc7accc711138addc738bd
Willex275 <willestrandberg14@gmail.com>
2018-09-21 14:39:07 +0200

    commit Abracadabra

---
+++ src/Kattis/StuckINaTimeLoop.java
@@ -0,0 +1,17 @@
+package Kattis;
+
+import java.util.Scanner;
+
+public class StuckINaTimeLoop {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int i = input.nextInt();
+		for (int j = 1; j <= i; j++) {
+			System.out.println(j + "Abracadabra");
+		}
+		
+	}
+
+}
