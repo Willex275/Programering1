9436c6e8191edc9685bf0e3ee7f60e41e45e15dc
Willex275 <willestrandberg14@gmail.com>
2018-09-21 14:32:13 +0200

    commit R2

--- src/Kattis/R2.java
+++ src/Kattis/R2.java
@@ -1,5 +1,22 @@
 package Kattis;
 
+import java.util.Scanner;
+
 public class R2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int R1 = input.nextInt();
+		
+		int S = input.nextInt();
+		
+		input.close();
+		
+		System.out.println(S * 2 - R1);
+		
+	}
+	
 
 }
