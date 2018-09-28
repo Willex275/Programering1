773fac801961b41763de4eca8664633a1d4f1d2c
Willex275 <willestrandberg14@gmail.com>
2018-09-28 14:10:16 +0200

    commit FizzBuzz

--- src/Kattis/FizzBuzz.java
+++ src/Kattis/FizzBuzz.java
@@ -8,6 +8,27 @@ public class FizzBuzz {
 		
 		Scanner input = new Scanner(System.in);
 		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		int n = input.nextInt();
+		 
+		for (int i = 1; i <= n;  i++) {
+			if(i % x == 0 && i % y == 0) {
+				System.out.println("Fizzbuzz");
+				
+				
+			}
+			else if (i % y == 0) {
+				System.out.println("Buzz");
+			}
+			else if (i % x == 0 ) {
+				System.out.println("Fizz");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+		
 		
 		
 		
