c2f22bdb911949d2c7a4da0e4c3252bbb0a2db8f
Willex275 <willestrandberg14@gmail.com>
2018-10-16 15:32:14 +0200

    commit 5 slut

--- src/läxor_till_github/uppgift_v41_rovarspraket.java
+++ src/läxor_till_github/uppgift_v41_rovarspraket.java
@@ -5,59 +5,53 @@ import java.util.Scanner;
 public class uppgift_v41_rovarspraket {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+		System.out.println(sjorovare(str));
 		
 		
 	}
 	public static String sjorovare(String str) {
 	      String mon = "";
-	      for(int i = 0; i < str.length(); i++)
+	      for(int i = 0; i < str.length(); i++) {
 	      switch (str.toLowerCase().charAt(i)) {
 	      
 	      case 'b':
-	      case '':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
+	      case 'c':
+	      case 'd':
+	      case 'f':
+	      case 'g':
+	      case 'h':
+	      case 'j':
+	      case 'k':
+	      case 'l':
+	      case 'm':
+	      case 'n':
+	      case 'p':
+	      case 'q':
+	      case 'r':
+	      case 's':
+	      case 't':
+	      case 'v':
+	      case 'w':
+	      case 'z':
 	    	  mon += str.charAt(i) + "o" + str.charAt(i); 
 	    	  
 	      }
 switch (str.toLowerCase().charAt(i)) {
 	      
-	      case 'b':
-	      case '':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
-	      case 'b':
+	      case 'a':
+	      case 'e':
+	      case 'i':
+	      case 'o':
+	      case 'u':
+	      case 'y':
+	      case 'å':
+	      case 'ä':
+	      case 'ö':
 	      case ' ':
 	    	  mon += str.charAt(i); 
 }
+	      }
 
 		
 		return mon;
