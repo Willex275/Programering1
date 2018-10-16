afd7e5fba5a397a255860ff7862f8888dee3e77e
Willex275 <willestrandberg14@gmail.com>
2018-10-16 14:29:24 +0200

    commit 2 uppgift 3 råkade döpa den föra commiten till uppgift 4 Igentligen uppgift3

--- src/läxor_till_github/uppgift_4_vecka41.java
+++ src/läxor_till_github/uppgift_4_vecka41.java
@@ -1,15 +1,21 @@
 package läxor_till_github;
 
+import java.util.Scanner;
+
 public class uppgift_4_vecka41 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println(count(sc.nextLine());
 		
 	}
 	
 	public static int count(String str, char c) {
-		int 
-		
-		
-		
+		int count = 0;
+		for(int i = 0; i < str.length();i++) {
+			if (str.charAt(i) == c) {
+			    count++;			}
+		}
+		return count;
 	}
 	
 	
