fc6c8e96d735b8ce29200313b6e4a55304f9a99b
Willex275 <willestrandberg14@gmail.com>
2018-10-16 14:39:49 +0200

    commit uppgift 3

--- src/läxor_till_github/uppgift_3_vecka41.java
+++ src/läxor_till_github/uppgift_3_vecka41.java
@@ -2,22 +2,20 @@ package läxor_till_github;
 
 import java.util.Scanner;
 
-public class uppgift_4_vecka41 {
+public class uppgift_3_vecka41 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println(count(sc.nextLine());
-		
+		System.out.println(count(input.nextLine(), input.next().charAt(0)));
+		input.close();
 	}
-	
 	public static int count(String str, char c) {
 		int count = 0;
 		for(int i = 0; i < str.length();i++) {
 			if (str.charAt(i) == c) {
-			    count++;			}
+			    count++;			
+			}
 		}
 		return count;
 	}
-	
-	
 
 }
