c49a6aff9549571ffb3a8e74f613922a555ba1d5
Willex275 <willestrandberg14@gmail.com>
2018-10-16 14:13:03 +0200

    commit 1 uppgift 4

---
+++ src/läxor_till_github/uppgift_4_vecka41.java
@@ -0,0 +1,17 @@
+package läxor_till_github;
+
+public class uppgift_4_vecka41 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int count(String str, char c) {
+		int 
+		
+		
+		
+	}
+	
+	
+
+}
