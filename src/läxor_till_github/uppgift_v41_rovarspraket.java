1a5b642ed54ad47b1ae417ced5288c48e2bc6090
Willex275 <willestrandberg14@gmail.com>
2018-10-16 14:52:42 +0200

    commit 1 rövarspråket

---
+++ src/läxor_till_github/uppgift_v41_rovarspraket.java
@@ -0,0 +1,16 @@
+package läxor_till_github;
+
+import java.util.Scanner;
+
+public class uppgift_v41_rovarspraket {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+	}
+	public static String sjorovare(String str) {
+		
+		
+	}
+
+}
