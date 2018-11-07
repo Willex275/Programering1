83005cffcf27b70da52cdf6c63501286537c8fc7
Willex275 <willestrandberg14@gmail.com>
2018-11-07 11:46:17 +0100

    commit 2 på Uppgifterna
    gjorde 3 uppgiften på 5 liter vatten

--- src/Enums/Method_Table.java
+++ src/Enums/Method_Table.java
@@ -3,6 +3,8 @@ package Enums;
 public class Method_Table {
 	public static void main(String[] args) {
 		System.out.println(volumeToMass(SolidTable.IRON, 1));
+		System.out.println(svtDistance(2.8,3600));
+		System.out.println(heat(FluidTable.WATER, 5, 1));
 	}
 	
 	public static double volumeToMass(SolidTable solid, double volume) {
@@ -11,5 +13,14 @@ public class Method_Table {
 			
 			
 		}
+	public static double svtDistance(double velocity, double time) {
+		double distance = velocity*time; // gora om 60 min till sek 60*60=3600. 
+		return distance;
+	}
+	
+	public static double heat(FluidTable fluid, double volume, double deltaT) {
+		double heat = volume* fluid.heatCapacity * deltaT;
+		return heat;
+	}
 
 }
