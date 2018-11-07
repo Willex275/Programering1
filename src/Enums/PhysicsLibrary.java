7c665e744c73db19f91d1a66c258c919cb789b45
Willex275 <willestrandberg14@gmail.com>
2018-11-07 08:47:43 +0100

    commit 2

--- src/Enums/PhysicsLibrary.java
+++ src/Enums/PhysicsLibrary.java
@@ -5,7 +5,8 @@ public class PhysicsLibrary {
 		
 		System.out.println(volumeToMass(SolidTable.IRON, 1));
 		
-	}
+		
+	} // En metod som raknar ut massan på ett amne
 	
 	public static double volumeToMass(SolidTable solid, double volume) {
 	 double mass = volume*solid.density;
@@ -13,7 +14,28 @@ public class PhysicsLibrary {
 		return	mass; 
 		
 		
+	} 
+	/**
+	 * 
+	 * @param velocity
+	 * @param time
+	 * @return
+	 */
+	//En metod som raknar ut tid för hjalp av stracka och hastighet
+
+	public static double svtDistance(double velocity, double time) {
+		
+		return velocity*time; 
+		 
 	}
+	//En metod som raknar ut hur mycket energi som gar at for att varme upp x volym vatska 
+	
+     public static double heat(FluidTable fluid, double volume, double deltaT) {
+    	 
+    	 return volume*deltaT;
+     }
+	
+	
 	
 
 }
