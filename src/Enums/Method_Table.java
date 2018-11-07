3dcc3e11cee41b3c3e87b344ca69cb23d04a674f
Willex275 <willestrandberg14@gmail.com>
2018-11-07 12:21:20 +0100

    commit 3 på uppgifterna

--- src/Enums/Method_Table.java
+++ src/Enums/Method_Table.java
@@ -1,10 +1,16 @@
 package Enums;
 
 public class Method_Table {
+	
+	static double G = 9.82;
+	static double ATM = 101.3E3;
+	
 	public static void main(String[] args) {
 		System.out.println(volumeToMass(SolidTable.IRON, 1));
 		System.out.println(svtDistance(2.8,3600));
 		System.out.println(heat(FluidTable.WATER, 5, 1));
+		System.out.println(pressureUnderWater(100)); // svar 979054pa 
+		System.out.println(velocityToHeight(50/3.6)+1.8);
 	}
 	
 	public static double volumeToMass(SolidTable solid, double volume) {
@@ -19,8 +25,22 @@ public class Method_Table {
 	}
 	
 	public static double heat(FluidTable fluid, double volume, double deltaT) {
-		double heat = volume* fluid.heatCapacity * deltaT;
+		double heat = volume* fluid.heatCapacity * deltaT * fluid.density;
 		return heat;
 	}
-
+	
+	public static double pressureUnderWater(double deep) {
+		double pressure = deep * G * 0.997 * 1E3;
+		return pressure;
+		
+	}
+	
+	public static double velocityToHeight(double velocity) {
+		double height = Math.pow (velocity,2)/(2*G);
+		return height; 
+	}
+	
+	
+	
+	
 }
