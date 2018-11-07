3b215b916d2b73c798ec77e078c475df68be53ca
Willex275 <willestrandberg14@gmail.com>
2018-11-07 10:18:22 +0100

    commit 5

--- src/Enums/PhysicsLibrary.java
+++ src/Enums/PhysicsLibrary.java
@@ -46,25 +46,48 @@ public class PhysicsLibrary {
     	 return (fahrenheit -32) / 1.8;
      }
      
-         // En metod som raknar ut vatsketrycket I en vatska vid ett vIsst dJup.
+         // En metod som raknar ut vatsketrycket I en vatska vid ett vIsst dJup. (5)
      public static double fluidPressure(FluidTable fluid, double deep) {
     	 
     	 return fluid.density * G * deep; 
      }
-	//En metod som raknar ut vatsketrycket i vatten vid ett visst djup.
+	//En metod som raknar ut vatsketrycket i vatten vid ett visst djup. (6)
      public static double pressureUnderWater(double deep) {
     	 return deep * G * 0.997 * 1E3;
      }
-	// gor om grader kelvin till grader celcius 
+	// gor om grader kelvin till grader celcius (7)
      public static double kelvinToCelsius(double kelvin) {
     	 double celsius = kelvin + 273.15; 
     	 return celsius; 
      }
-     // En metod som raknar ut kinetisk energi med hjalp av massa och hastighet.
+     // En metod som raknar ut kinetisk energi med hjalp av massa och hastighet.(8)
      public static double kineticEnergy(double mass, double velocity) {
     	 
     	 return 0.5 * mass * velocity;
      }
+     // En metod som raknar ut potentiell energi med hjalp av massa och hojd (9)
+     public static double potentialEnergy(double mass, double height) {
+    	
+    	 return mass * 9.82 * height;
+     }
+     // En metod som raknar ut hur hog hastighet man kommer upp i som man (10)
+     // slapper ett foremål från en viss hojd.
+     public static double fallSpeed(double height) {
+    	 
+    	 return Math.sqrt(2*G*height);
+     }
+     // metod (11) En metod som raknar ut skillnad mellan tva givna varden.
+     public static double delta(double first, double last) {
+    	 
+    	 return last - first;
+     }
+     // metod 12 En metod som gor om en viss volym av gas till en massa.
+     public static double volumeToMass(GasTable gas, double volume) {
+    	 
+    	 return volume * gas.density;
+     }
+     
+    
 }
 
      
