9ef9ee55a62449203347f75998de121cf08958e5
Willex275 <willestrandberg14@gmail.com>
2018-11-07 14:52:44 +0100

    commit små ändringar

--- src/Enums/Method_Table.java
+++ src/Enums/Method_Table.java
@@ -6,21 +6,21 @@ public class Method_Table {
 	static double ATM = 101.3E3;
 	
 	public static void main(String[] args) {
-		System.out.println(volumeToMass(SolidTable.IRON, 1));
-		System.out.println(svtDistance(2.8,3600));
-		System.out.println(heat(FluidTable.WATER, 5, 1));
-		System.out.println(pressureUnderWater(100)); // svar 979054pa 
-		System.out.println(velocityToHeight(50/3.6)+1.8);
-		System.out.println(PhysicsLibrary.power(PhysicsLibrary.work(740, 100), 4.4));// uppgift 6
+		System.out.println(volumeToMass(SolidTable.IRON, 1)+ " Density");
+		System.out.println(svtDistance(2.8,3600)+ " meter");
+		System.out.println(heat(FluidTable.WATER, 0.005, 1)+ " J");
+		System.out.println(pressureUnderWater(100)+ " pa"); // svar 979054pa 
+		System.out.println((velocityToHeight(50/3.6)+1.8)+" m");
+		System.out.println(PhysicsLibrary.power(PhysicsLibrary.work(740, 100), 4.4)+ " Watt");// uppgift 6
         double height = 10;
 		int s = 0;
 		while(PhysicsLibrary.velocityToHeight(PhysicsLibrary.fallSpeed(height)) > 0.5) {
 			height = height*0.99;
-			s++; }System.out.println(s);// metoden raknar ut hur manga ganger bollen hinner stutsa innan energi
+			s++; }System.out.println((s)+" gånger");// metoden raknar ut hur manga ganger bollen hinner stutsa innan energi
 			//tar slut när Bollen minskar med 1% efter varje stuts mot marken
-		System.out.println(PhysicsLibrary.work(700*30, 100));// räknar arbete med hjälp av Massa*a Sträckan	
+		System.out.println(PhysicsLibrary.work(700*30, 100)+ " ARBETE");// räknar arbete med hjälp av Massa*a Sträckan	
 		System.out.println((PhysicsLibrary.heat(SolidTable.ICE, 0.5,5)+2260*1E3*0.5) + "J");
-		System.out.println(PhysicsLibrary.fallSpeed(100) + " m/s");
+		System.out.println(PhysicsLibrary.fallSpeed(100) + " m/s"); 
 		
 		
 			
@@ -38,7 +38,7 @@ public class Method_Table {
 	}
 	
 	public static double heat(FluidTable fluid, double volume, double deltaT) {
-		double heat = volume* fluid.heatCapacity * deltaT * fluid.density;
+		double heat = volume*fluid.density* fluid.heatCapacity * deltaT;
 		return heat;
 	}
 	
